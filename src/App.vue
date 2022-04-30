8a6355e8562b30f8433d76f7cc7d675a02d9d370
Arseniy Bogachev <senya.boagchev@mail.ru>
2022-04-30 19:45:41 +0300

    computed create

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,16 @@
       <MySelect v-model="selected_value" v-bind:option_list="option_list"/>
     </div>
     <ul v-if="women.length">
-      <NameApp v-for="(w, i) in women"
-               :key="w.id" v-bind:w="w"
+      <transition-group>
+        <NameApp v-for="(w, i) in sort_array"
+               :key="w.id"
+               v-bind:w="w"
                v-bind:i="i"
                v-on:deleted="deleted"
                v-on:display="display"
                v-on:update="update"
       />
+      </transition-group>
     </ul>
     <div v-else>List person empty.</div>
   </div>
@@ -81,6 +84,29 @@ export default {
     NameApp,
     FormApp
   },
+  computed:{
+    sort_array(){
+      if (this.selected_value === 'name'){
+        return [...this.women].sort((str1, str2) => (str1.name).localeCompare(str2.name))
+      }
+      else if (this.selected_value === 'age') {
+        return [...this.women].sort((int1, int2) => int1.age - int2.age)
+      }
+      else {
+        return this.women
+      }
+    }
+  },
+  // watch:{
+  //   selected_value(value){
+  //     if (value === 'name'){
+  //       this.women.sort((str1, str2) => (str1.name).localeCompare(str2.name))
+  //     }
+  //     else {
+  //       this.women.sort((int1, int2) => int1.age - int2.age)
+  //     }
+  //   }
+  // },
 }
 </script>
 
